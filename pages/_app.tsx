c2417a92e377bed6caa32b40a8b281bbbe1b8811
Mark Logan <mark@gmail.com>
2019-12-12 10:58:13 -0800

    s/site/app/

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default class extends App {
     const {props} = this as any
     const {Component, pageProps} = props
 
-    const siteId = process.env.UM_SITE_ID
+    const appId = process.env.UM_APP_ID
 
     // Usermatic components are formatted with bootstrap classes. They don't
     // depend on any bootstrap javascript, so you can use them without bootstrap.
@@ -18,7 +18,7 @@ export default class extends App {
         <title>Usermatic Example Application</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
       </Head>
-      <UsermaticAuthProvider siteId={siteId} uri="http://api.usermatic.local:3002/graphql">
+      <UsermaticAuthProvider appId={appId} uri="http://api.usermatic.local:3002/graphql">
         <Component {...pageProps} />
       </UsermaticAuthProvider>
     </>
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken'
 const verifyJwt = (authHeader: string | undefined, secret: string) => {
   if (!authHeader) { return undefined }
   try {
-    return jwt.verify(authHeader, process.env.UM_SITE_SECRET)
+    return jwt.verify(authHeader, process.env.UM_APP_SECRET)
   } catch (err) {
     console.error(err)
     return undefined
@@ -16,15 +16,15 @@ export default (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   const { authorization } = req.headers
 
-  if (!process.env.UM_SITE_SECRET) {
+  if (!process.env.UM_APP_SECRET) {
     res.statusCode = 500
-    const message = "No UM_SITE_SECRET variable found in process.env, can't authenticate client"
+    const message = "No UM_APP_SECRET variable found in process.env, can't authenticate client"
     console.error(message)
     res.end(JSON.stringify({ message }))
     return
   }
 
-  const authToken = verifyJwt(authorization, process.env.UM_SITE_SECRET);
+  const authToken = verifyJwt(authorization, process.env.UM_APP_SECRET);
 
   if (authToken) {
     res.statusCode = 200
